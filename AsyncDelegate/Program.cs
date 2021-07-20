77d413ad5c2d7fc81b4d5ff1a600f5996aa1b8e8
[Martijen] <jennifer_martin@outlook.fr>
2021-07-20 18:30:46 +0200

    Ajoutez des fichiers projet.

---
+++ AsyncDelegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+        static bool downloading = false;
+        static void Main(string[] args)
+        {
+            var webclient = new WebClient();
+
+            Console.Write("Téléchargement...");
+            string url = "https://codeavecjonathan.com/res/bateau.jpg";
+            //webclient.DownloadFile(url, "bateau.jpg");
+
+            downloading = true;
+            webclient.DownloadFileCompleted += Webclient_DownloadFileCompleted;
+            webclient.DownloadFileAsync(new Uri(url), "bateau.jpg");
+
+            while (downloading)
+            {
+                Thread.Sleep(500);
+                if (downloading)
+                {
+                    Console.Write(".");
+                } 
+            }
+
+            Console.WriteLine("FIN DU PROGRAMME");
+        }
+
+        private static void Webclient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Téléchargement terminé");
+            downloading = false;
+        }
+
+    }
+}
